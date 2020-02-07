9ccb3e0f195ead1e60ea3fb912e08ba2430ef25e
mohammad.shobeiri@gmail.com <mohammad.shobeiri@gmail.com>
2020-02-07 14:10:16 +0330

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+var CryptoJS = require("crypto-js");
+var request = require('request');
+
+exports.sadadPayment = async function ({TerminalId,MerchantId,MerchantKey,Amount,OrderId,ReturnUrl}) {
+    const dataBytes = Buffer.from(`${TerminalId};${OrderId};${Amount}`).toString()
+    var iv = CryptoJS.lib.WordArray.create([0,0]);
+    const signDataByte = CryptoJS.TripleDES.encrypt(dataBytes,CryptoJS.enc.Base64.parse(MerchantKey),{
+      mode: CryptoJS.mode.ECB,
+      padding: CryptoJS.pad.Pkcs7,
+      iv
+    })
+    const SignData = CryptoJS.enc.Base64.stringify(signDataByte.ciphertext);
+    debugger
+    request.post({
+        headers: {
+            'content-type': 'application/json'
+        },
+        url: 'https://sadad.shaparak.ir/api/v0/Request/PaymentRequest',
+        body: JSON.stringify({
+            TerminalId,
+            MerchantId,
+            Amount,
+            SignData,
+            ReturnUrl,
+            LocalDateTime: new Date(),
+            OrderId,
+        })
+    }, function (error, response, body) {
+        debugger
+    });
+}
