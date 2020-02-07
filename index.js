19c8f86b34f39216c0b016a4d3126b49fe26ba04
sms1989 <mohammad.shobeiri@live.com>
2020-02-07 11:04:04 +0000

    add Promise

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var CryptoJS = require("crypto-js");
 var request = require('request');
 
-exports.sadadPayment = async function ({TerminalId,MerchantId,MerchantKey,Amount,OrderId,ReturnUrl}) {
+exports.sadadPayment = function ({TerminalId,MerchantId,MerchantKey,Amount,OrderId,ReturnUrl}) {
     const dataBytes = Buffer.from(`${TerminalId};${OrderId};${Amount}`).toString()
     var iv = CryptoJS.lib.WordArray.create([0,0]);
     const signDataByte = CryptoJS.TripleDES.encrypt(dataBytes,CryptoJS.enc.Base64.parse(MerchantKey),{
@@ -10,22 +10,23 @@ exports.sadadPayment = async function ({TerminalId,MerchantId,MerchantKey,Amount
       iv
     })
     const SignData = CryptoJS.enc.Base64.stringify(signDataByte.ciphertext);
-    debugger
-    request.post({
-        headers: {
-            'content-type': 'application/json'
-        },
-        url: 'https://sadad.shaparak.ir/api/v0/Request/PaymentRequest',
-        body: JSON.stringify({
-            TerminalId,
-            MerchantId,
-            Amount,
-            SignData,
-            ReturnUrl,
-            LocalDateTime: new Date(),
-            OrderId,
-        })
-    }, function (error, response, body) {
-        debugger
-    });
+    return new Promise(function (resolve,reject) {
+        request.post({
+            headers: {
+                'content-type': 'application/json'
+            },
+            url: 'https://sadad.shaparak.ir/api/v0/Request/PaymentRequest',
+            body: JSON.stringify({
+                TerminalId,
+                MerchantId,
+                Amount,
+                SignData,
+                ReturnUrl,
+                LocalDateTime: new Date(),
+                OrderId,
+            })
+        }, function (error, response, body) {
+            resolve(JSON.parse(body))
+        });
+    })
 }
